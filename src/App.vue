c66ca6ddb9c2f55b4ebd6e422a0b059acb0af639
haohe <hao503106@163.com>
2018-08-23 11:32:12 +0800

    实现登录后出发获取菜单和路由

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,63 @@
             |
             <router-link to="/login">login</router-link>
         </div>
-        <router-view/>
+        <!-- 触发器要写在路由引入部分上面 -->
+        <router-view @login="loginAss" @logout="logoutAss"/>
     </div>
 </template>
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
 
+    @Component
+    export default class app extends Vue {
+        // private userData: object = null;
+        // private menuData: object = null;
+
+        private loginAss() {
+            this.$message({
+                message: '准备获取ass',
+                type: 'success',
+                duration: 0, // 不自动关闭
+                showClose: true, // 显示关闭按钮
+            })
+            if (sessionStorage.getItem('user-token')) {
+                // console.log('token：' + sessionStorage.getItem('user-token'));
+
+            } else {
+                this.$router.replace('/login');
+            }
+        }
+
+        private logoutAss() {
+            this.$message({
+                message: '准备登出',
+                type: 'success',
+                duration: 0, // 不自动关闭
+                showClose: true, // 显示关闭按钮
+            });
+        }
+    }
+
+    // @Component({
+    //     data(){
+    //         return {
+    //             userData:null,
+    //             menuData:null,
+    //         }
+    //     },
+    //     methods:{
+    //         loginAss: function ():void {
+    //             this.$message({
+    //                 message: '登录成功',
+    //                 type: 'success',
+    //                 duration: 0, // 不自动关闭
+    //                 showClose: true, // 显示关闭按钮
+    //             })
+    //         }
+    //     }
+    //
+    // })
+</script>
 <style lang="less">
     @import "assets/common.css";
 
